1313d25e17d5b09a407c6e26b350afd219263cb3
Matheus Antonio <matheus.silva-loldesign@fretebras.com.br>
2022-05-07 11:53:04 -0300

    feat: add authenticate pages

--- src/Routes/allRoutes.js
+++ src/Routes/allRoutes.js
@@ -5,9 +5,19 @@ import { Redirect } from 'react-router-dom';
 import FinanceDashboard from '../pages/Finance/FinanceDashboard';
 import Modal from '../pages/Finance/UiModals';
 
+//Authentication pages
+import BasicSignIn from '../pages/Authentication/Login';
+// import CoverSignIn from '../pages/Authentication/Login/CoverSignIn';
+import BasicSignUp from '../pages/Authentication/Register';
+// import CoverSignUp from '../pages/Authentication/Register/CoverSignUp';
+import BasicPasswReset from '../pages/Authentication/ForgetPassword';
+
 const authProtectedRoutes = [
   //Pages
   { path: '/finance', component: FinanceDashboard },
+  { path: '/login', component: BasicSignIn },
+  { path: '/register', component: BasicSignUp },
+  { path: '/passwordReset', component: BasicPasswReset },
   { path: '/labs', component: Modal },
 
   // this route should be at the end of all other routes
--- src/pages/Finance/UiModals.js
+++ src/pages/Finance/UiModals.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
-import { Button, Card, CardBody, Col, Modal, ModalHeader } from 'reactstrap';
+import {
+  Button,
+  Card,
+  CardBody,
+  Col,
+  Modal,
+  ModalHeader,
+  Input,
+} from 'reactstrap';
 import PreviewCardHeader from '../../Components/Common/PreviewCardHeader';
-import { AnimationModalExample } from './UiModalCode';
 
 const UiModals = () => {
   const [modal_animationZoom, setmodal_animationZoom] = useState(false);
   function tog_animationZoom() {
     setmodal_animationZoom(!modal_animationZoom);
+  }
+  const [modal_grid, setmodal_grid] = useState(false);
+  function tog_grid() {
+    setmodal_grid(!modal_grid);
   }
 
   document.title = 'Modals | Velzon - React Admin & Dashboard Template';
@@ -16,62 +27,134 @@ const UiModals = () => {
         <Card>
           <PreviewCardHeader title='Animation Modals' />
           <CardBody>
-            <div className='live-preview'>
-              <div>
-                <div className='hstack gap-2 flex-wrap'>
-                  <Button color='primary' onClick={() => tog_animationZoom()}>
-                    Zoom In Modal
-                  </Button>
-                </div>
-              </div>
-            </div>
-            <div className='d-none code-view'>
-              <pre className='language-markup' style={{ height: '275px' }}>
-                <code>
-                  <AnimationModalExample />
-                </code>
-              </pre>
+            <div className='hstack gap-2 flex-wrap'>
+              <Button color='primary' onClick={() => tog_grid()}>
+                Zoom In Modal
+              </Button>
             </div>
           </CardBody>
         </Card>
       </Col>
-      {/* ZoomIn Animation */}
       <Modal
-        id='flipModal'
-        isOpen={modal_animationZoom}
+        isOpen={modal_grid}
         toggle={() => {
-          tog_animationZoom();
+          tog_grid();
         }}
-        modalClassName='zoomIn'
-        centered
       >
-        <ModalHeader>Modal Heading</ModalHeader>
-        <div className='modal-body'>
-          <h5 className='fs-16'>Overflowing text to show scroll behavior</h5>
-          <p className='text-muted'>
-            One morning, when Gregor Samsa woke from troubled dreams, he found
-            himself transformed in his bed into a horrible vermin. He lay on his
-            armour-like back, and if he lifted his head a little he could see
-            his brown belly, slightly domed and divided by arches into stiff
-            sections.
-          </p>
-          <p className='text-muted'>
-            The bedding was hardly able to cover it and seemed ready to slide
-            off any moment. His many legs, pitifully thin compared with the size
-            of the rest of him, waved about helplessly as he looked. "What's
-            happened to me?" he thought.
-          </p>
-        </div>
-        <div className='modal-footer'>
+        <ModalHeader>
+          Grid Modals
           <Button
-            color='light'
+            type='button'
             onClick={() => {
-              tog_animationZoom();
+              setmodal_grid(false);
             }}
-          >
-            Close
-          </Button>
-          <Button color='primary'> Save changes </Button>
+            className='btn-close'
+            aria-label='Close'
+          ></Button>
+        </ModalHeader>
+        <div className='modal-body'>
+          <form action='#'>
+            <div className='row g-3'>
+              <Col xxl={6}>
+                <div>
+                  <label htmlFor='firstName' className='form-label'>
+                    First Name
+                  </label>
+                  <Input
+                    type='text'
+                    className='form-control'
+                    id='firstName'
+                    placeholder='Enter firstname'
+                  />
+                </div>
+              </Col>
+              <Col xxl={6}>
+                <div>
+                  <label htmlFor='lastName' className='form-label'>
+                    Last Name
+                  </label>
+                  <Input
+                    type='text'
+                    className='form-control'
+                    id='lastName'
+                    placeholder='Enter lastname'
+                  />
+                </div>
+              </Col>
+              <Col lg={12}>
+                <label className='form-label'>Gender</label>
+                <div>
+                  <div className='form-check form-check-inline'>
+                    <Input
+                      className='form-check-input'
+                      type='radio'
+                      name='inlineRadioOptions'
+                      id='inlineRadio1'
+                      value='option1'
+                    />
+                    <label className='form-check-label' htmlFor='inlineRadio1'>
+                      Male
+                    </label>
+                  </div>
+                  <div className='form-check form-check-inline'>
+                    <Input
+                      className='form-check-input'
+                      type='radio'
+                      name='inlineRadioOptions'
+                      id='inlineRadio2'
+                      value='option2'
+                    />
+                    <label className='form-check-label' htmlFor='inlineRadio2'>
+                      Female
+                    </label>
+                  </div>
+                  <div className='form-check form-check-inline'>
+                    <Input
+                      className='form-check-input'
+                      type='radio'
+                      name='inlineRadioOptions'
+                      id='inlineRadio3'
+                      value='option3'
+                    />
+                    <label className='form-check-label' htmlFor='inlineRadio3'>
+                      Others
+                    </label>
+                  </div>
+                </div>
+              </Col>
+              <Col xxl={6}>
+                <label htmlFor='emailInput' className='form-label'>
+                  Email
+                </label>
+                <Input
+                  type='email'
+                  className='form-control'
+                  id='emailInput'
+                  placeholder='Enter your email'
+                />
+              </Col>
+              <Col xxl={6}>
+                <label htmlFor='passwordInput' className='form-label'>
+                  Password
+                </label>
+                <Input
+                  type='password'
+                  className='form-control'
+                  id='passwordInput'
+                  value='451326546'
+                  readOnly
+                />
+              </Col>
+              <Col lg={12}>
+                <div className='hstack gap-2 justify-content-end'>
+                  <Button color='light' onClick={() => setmodal_grid(false)}>
+                    Close
+                  </Button>
+                  <Button color='primary'>Submit</Button>
+                </div>
+              </Col>
+            </div>
+          </form>
         </div>
       </Modal>
     </React.Fragment>
